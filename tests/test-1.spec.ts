1b74cbfe86d1ad04753442c24e16fc8a68352e9c
Mikemgbeck <mikemgbeck@gmail.com>
2024-08-16 08:26:05 -0600

    test changes

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,11 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/search?q=localhost%3A%2F%2F5174&oq=localhost%3A%2F%2F5174&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg60gEINTU3M2owajKoAgCwAgE&sourceid=chrome&ie=UTF-8');
-  await page.goto('http://localhost:5174/');
-  await page.getByRole('row', { name: 'matt s matt@gmail.com Edit Delete' }).locator('a').click();
-  await page.getByPlaceholder('Last Name').click();
-  await page.getByPlaceholder('Last Name').fill('sieber');
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await page.getByRole('button', { name: 'Cancel' }).click();
+await page.goto('http://localhost:5174/');
+await page.getByRole('row', { name: 'matt s matt@gmail.com Edit Delete' }).locator('a').click();
+await page.getByPlaceholder('Last Name').click();
+await page.getByPlaceholder('Last Name').fill('sieber');
+await page.getByRole('button', { name: 'Submit' }).click();
+await page.getByRole('row', { name: '8 matt sieber matt@gmail.com' }).locator('a').click();
+await page.getByPlaceholder('Last Name').click();
+await page.getByPlaceholder('Last Name').click();
+await page.getByPlaceholder('Last Name').fill('s');
+await page.getByRole('button', { name: 'Submit' }).click();
 });
